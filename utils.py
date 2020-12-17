a10a04c4a1813caf2f1f34e6cfb82715b9be4148
Adam Kowalchyk <akowalchyk@zagmail.gonzaga.edu>
2020-12-17 11:30:48 -0800

    Final Commit

--- utils.py
+++ utils.py
@@ -2,6 +2,61 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def play_duration_milliseconds_to_hours(df):
+    df = df.rename(columns = {"Play Duration Milliseconds":"Play Duration Hours"}) 
+    ser = df["Play Duration Hours"].copy()
+    new_ser = []
+    for i in range(0, len(ser)):
+        new_ser.append((ser[i]/(1000*60*60)) % 24)
+    df["Play Duration Hours"] = new_ser  
+
+def reason_for_end_cleaning(df):
+    ser = df["End Reason Type"].copy()
+    for i in range(0, len(ser)):
+        curr = ser[i]
+        if (curr == "TRACK_SKIPPED_FORWARDS" or curr == "TRACK_SKIPPED_BACKWARDS" or 
+        curr == "MANUALLY_SELECTED_PLAYBACK_OF_A_DIFF_ITEM"):
+            ser[i] = "Skipped"
+        elif (curr == "NATURAL_END_OF_TRACK" or curr == "SCRUB_END"):
+            ser[i] = "NATURAL_END_OF_TRACK"
+        elif (curr == "PLAYBACK_MANUALLY_PAUSED"):
+            ser[i] = "Paused"
+        else:
+            ser[i] = "Scrubbed to Beginning"
+    df["End Reason Type"] = ser
+
+def add_month_attribute(df):
+    month = []
+    dates_array = df["Event Start Timestamp"]
+    for dates in dates_array:
+        if (dates[0:7]== "2019-10"):
+            month.append("Oct/2019")
+        if (dates[0:7] == "2019-11"):
+            month.append("Nov/2019")
+        if (dates[0:7] == "2019-12"):
+            month.append("Dec/2019")
+        if (dates[0:7] == "2020-01"):
+            month.append("Jan/2020")
+        if (dates[0:7] == "2020-02"):
+            month.append("Feb/2020")
+        if (dates[0:7] == "2020-03"):
+            month.append("Mar/2020")
+        if (dates[0:7] == "2020-04"):
+            month.append("Apr/2020")
+        if (dates[0:7] == "2020-05"):
+            month.append("May/2020")
+        if (dates[0:7] == "2020-06"):
+            month.append("June/2020")
+        if (dates[0:7] == "2020-07"):
+            month.append("July/2020")
+        if (dates[0:7] == "2020-08"):
+            month.append("Aug/2020")
+        if (dates[0:7] == "2020-09"):
+            month.append("Sept/2020")
+        if (dates[0:7] == "2020-10"):
+            month.append("Oct/2020")
+    df["month"] = month
+
 def Song_Counts_by_device(x,y):
     plt.figure()
     plt.bar(x, y)
